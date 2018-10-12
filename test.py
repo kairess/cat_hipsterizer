1096ba92d1a784d8ee52fa1fd0ea0f0a127f65d7
Brad <th.lee@ecubelabs.com>
2018-10-13 04:42:44 +0900

    add glasses

--- test.py
+++ test.py
@@ -2,10 +2,14 @@ import keras, sys, cv2, os
 from keras.models import Model, load_model
 import numpy as np
 import pandas as pd
+from math import atan2, degrees
 
 img_size = 224
 base_path = 'samples'
 file_list = sorted(os.listdir(base_path))
+
+# this is most important thing
+glasses = cv2.imread('images/glasses.png', cv2.IMREAD_UNCHANGED)
 
 bbs_model_name = sys.argv[1]
 lmks_model_name = sys.argv[2]
@@ -26,12 +30,46 @@ def resize_img(im):
       value=[0, 0, 0])
   return new_im, ratio, top, left
 
+# overlay function
+def overlay_transparent(background_img, img_to_overlay_t, x, y, overlay_size=None):
+  bg_img = background_img.copy()
+  # convert 3 channels to 4 channels
+  if bg_img.shape[2] == 3:
+    bg_img = cv2.cvtColor(bg_img, cv2.COLOR_BGR2BGRA)
+
+  if overlay_size is not None:
+    img_to_overlay_t = cv2.resize(img_to_overlay_t.copy(), overlay_size)
+
+  b, g, r, a = cv2.split(img_to_overlay_t)
+
+  mask = cv2.medianBlur(a, 5)
+
+  h, w, _ = img_to_overlay_t.shape
+  roi = bg_img[int(y-h/2):int(y+h/2), int(x-w/2):int(x+w/2)]
+
+  img1_bg = cv2.bitwise_and(roi.copy(), roi.copy(), mask=cv2.bitwise_not(mask))
+  img2_fg = cv2.bitwise_and(img_to_overlay_t, img_to_overlay_t, mask=mask)
+
+  bg_img[int(y-h/2):int(y+h/2), int(x-w/2):int(x+w/2)] = cv2.add(img1_bg, img2_fg)
+
+  # convert 4 channels to 4 channels
+  bg_img = cv2.cvtColor(bg_img, cv2.COLOR_BGRA2BGR)
+
+  return bg_img
+
+def angle_between(p1, p2):
+  xDiff = p2[0] - p1[0]
+  yDiff = p2[1] - p1[1]
+  return degrees(atan2(yDiff, xDiff))
+
+# testing
 for f in file_list:
   if '.jpg' not in f:
     continue
 
   img = cv2.imread(os.path.join(base_path, f))
   ori_img = img.copy()
+  result_img = img.copy()
 
   # predict bounding box
   img, ratio, top, left = resize_img(img)
@@ -66,10 +104,25 @@ for f in file_list:
   # visualize
   cv2.rectangle(ori_img, pt1=tuple(ori_bb[0]), pt2=tuple(ori_bb[1]), color=(255, 255, 255), thickness=2)
 
-  for l in ori_lmks:
+  for i, l in enumerate(ori_lmks):
+    cv2.putText(ori_img, str(i), tuple(l), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
     cv2.circle(ori_img, center=tuple(l), radius=1, color=(255, 255, 255), thickness=2)
 
+  # wearing glasses
+  glasses_center = np.mean([ori_lmks[0], ori_lmks[1]], axis=0)
+  glasses_size = np.linalg.norm(ori_lmks[0] - ori_lmks[1]) * 2
+  
+  angle = -angle_between(ori_lmks[0], ori_lmks[1])
+  M = cv2.getRotationMatrix2D((glasses.shape[1] / 2, glasses.shape[0] / 2), angle, 1)
+  rotated_glasses = cv2.warpAffine(glasses, M, (glasses.shape[1],glasses.shape[0]))
+
+  try:
+    result_img = overlay_transparent(result_img, rotated_glasses, glasses_center[0], glasses_center[1], overlay_size=(int(glasses_size), int(glasses.shape[0] * glasses_size / glasses.shape[1])))
+  except:
+    print('failed overlay image')
+
   cv2.imshow('img', ori_img)
+  cv2.imshow('result', result_img)
 
   if cv2.waitKey(0) == ord('q'):
     break
