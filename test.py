240afef141eb188380e04e7f7c1c4c6205a7ff64
Brad <th.lee@ecubelabs.com>
2018-10-13 03:32:02 +0900

    add landmark prediction to test

--- test.py
+++ test.py
@@ -7,8 +7,10 @@ img_size = 224
 base_path = 'samples'
 file_list = sorted(os.listdir(base_path))
 
-model_name = sys.argv[1]
-model = load_model(model_name)
+bbs_model_name = sys.argv[1]
+lmks_model_name = sys.argv[2]
+bbs_model = load_model(bbs_model_name)
+lmks_model = load_model(lmks_model_name)
 
 def resize_img(im):
   old_size = im.shape[:2] # old_size is in (height, width) format
@@ -29,15 +31,45 @@ for f in file_list:
     continue
 
   img = cv2.imread(os.path.join(base_path, f))
+  ori_img = img.copy()
 
-  # resize image and relocate landmarks
+  # predict bounding box
   img, ratio, top, left = resize_img(img)
 
   inputs = (img.astype('float32') / 255).reshape((1, img_size, img_size, 3))
-  bb = model.predict(inputs)[0].reshape((-1, 2))
+  pred_bb = bbs_model.predict(inputs)[0].reshape((-1, 2))
 
-  cv2.rectangle(img, pt1=tuple(bb[0]), pt2=tuple(bb[1]), color=(255, 255, 255), thickness=2)
-  cv2.imshow('img', img)
+  # compute bounding box of original image
+  ori_bb = ((pred_bb - np.array([left, top])) / ratio).astype(np.int)
+
+  # compute lazy bounding box for detecting landmarks
+  center = np.mean(ori_bb, axis=0)
+  face_size = max(np.abs(ori_bb[1] - ori_bb[0]))
+  new_bb = np.array([
+    center - face_size * 0.6,
+    center + face_size * 0.6
+  ]).astype(np.int)
+  new_bb = np.clip(new_bb, 0, 99999)
+
+  # predict landmarks
+  face_img = ori_img[new_bb[0][1]:new_bb[1][1], new_bb[0][0]:new_bb[1][0]]
+  face_img, face_ratio, face_top, face_left = resize_img(face_img)
+
+  face_inputs = (face_img.astype('float32') / 255).reshape((1, img_size, img_size, 3))
+
+  pred_lmks = lmks_model.predict(face_inputs)[0].reshape((-1, 2))
+
+  # compute landmark of original image
+  new_lmks = ((pred_lmks - np.array([face_left, face_top])) / face_ratio).astype(np.int)
+  ori_lmks = new_lmks + new_bb[0]
+
+  # visualize
+  cv2.rectangle(ori_img, pt1=tuple(ori_bb[0]), pt2=tuple(ori_bb[1]), color=(255, 255, 255), thickness=2)
+
+  for l in ori_lmks:
+    cv2.circle(ori_img, center=tuple(l), radius=1, color=(255, 255, 255), thickness=2)
+
+  cv2.imshow('img', ori_img)
 
   if cv2.waitKey(0) == ord('q'):
     break
